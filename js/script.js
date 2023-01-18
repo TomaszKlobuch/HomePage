748c535cb1c617866c38a9d41d5749d9810cd301
Tomasz Klobuch <tomaszklobuch@gmail.com>
2023-01-18 21:56:22 +0100

    JS function added / refactoin code.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
-let textSizeButton = document.querySelector(".js-textSize");
-let textSizeButtonSecond = document.querySelector(".js-textSize");
-let bodyTextSize = document.querySelector(".body");
-let styleButton = document.querySelector(".js-styleColor");
-let styleButtonSecond = document.querySelector(".js-styleColor");
-let bodyStyle = document.querySelector(".js-body");
-let navDivStyleSecond = document.querySelector(".js-nav__div--style");
-let navDivStyle = document.querySelectorAll(".js-nav__div");
-let navLinkStyle = document.querySelectorAll(".js-nav__link");
-let tableCellHeader = document.querySelectorAll(".js-table__cell--header");
+{
+  const welcome = () => {
+    console.log("Witam wszystkich / Hi everyone");
+  }
 
-textSizeButton.addEventListener("click", () => {
-  bodyTextSize.classList.toggle("body--textSize");
-});
+  welcome()
 
-styleButton.addEventListener("click", () => {
-  bodyStyle.classList.toggle("bodyDark");
-  navDivStyleSecond.classList.toggle("navDark__div--style");
-  textSizeButtonSecond.classList.toggle("navDark__button");
-  styleButtonSecond.classList.toggle("navDark__button");
-  for (let element of navDivStyle) {
-    element.classList.toggle("navDark__div")
+  const toggleBackground = () => {
+    const styleButtonSecond = document.querySelector(".js-styleColor");
+    const bodyStyle = document.querySelector(".js-body");
+    const navDivStyleSecond = document.querySelector(".js-nav__div--style");
+    const navDivStyle = document.querySelector(".js-nav__div");
+    const navLinkStyle = document.querySelector(".js-nav__link");
+    const tableCellHeader = document.querySelector(".js-table__cell--header");
+    const textSizeButtonSecond = document.querySelector(".js-textSize");
+
+    bodyStyle.classList.toggle("bodyDark");
+    navDivStyleSecond.classList.toggle("navDark__div--style");
+    textSizeButtonSecond.classList.toggle("navDark__button");
+    styleButtonSecond.classList.toggle("navDark__button");
+    navDivStyle.classList.toggle("navDark__div");
+    navLinkStyle.classList.toggle("navDark__link");
+    tableCellHeader.classList.toggle("tableDark__cell--header");
   }
-  for (let element of navLinkStyle) {
-    element.classList.toggle("navDark__link")
+
+  const toggleBackgroundButton = () => {
+    const styleButton = document.querySelector(".js-styleColor");
+    styleButton.addEventListener("click", toggleBackground);
   }
-  for (let element of tableCellHeader) {
-    element.classList.toggle("tableDark__cell--header")
+
+  toggleBackgroundButton();
+
+  
+
+  const toggleSize = () => {
+    const bodyTextSize = document.querySelector(".body")
+    bodyTextSize.classList.toggle("body--textSize");
   }
-});
+
+  const toggleSizebutton = () => {
+  const textSizeButton = document.querySelector(".js-textSize");
+  textSizeButton.addEventListener("click", (toggleSize))
+  };
+
+  toggleSizebutton();
+}
